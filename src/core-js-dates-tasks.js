132fdab3ddc3c77281950e4cca14d37ddb0b7137
Kate Mashko <katika.life@gmail.com>
2024-03-23 16:01:30 +0300

    feat: add solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,8 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  return new Date(date).toTimeString().split(' ')[0];
 }
 
 /**
@@ -46,8 +46,35 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const day = new Date(date).getDay();
+  let res;
+  switch (day) {
+    case 0:
+      res = 'Sunday';
+      break;
+    case 1:
+      res = 'Monday';
+      break;
+    case 2:
+      res = 'Tuesday';
+      break;
+    case 3:
+      res = 'Wednesday';
+      break;
+    case 4:
+      res = 'Thursday';
+      break;
+    case 5:
+      res = 'Friday';
+      break;
+    case 6:
+      res = 'Saturday';
+      break;
+    default:
+      res = 'no day';
+  }
+  return res;
 }
 
 /**
